b89f3cc47d50dbc012c7e0d528237e0019d8a2f4
Matheus Henrique <matheushds.br@gmail.com>
2020-06-09 14:20:39 -0300

    feat(painel): add painel

---
+++ painel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <title>Register</title>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 form-div login">
+       <div class="alert alert-success">Você está logado!</div>
+
+       <h3>Bem vindo, nome</h3>
+
+       <p><a href="#" class="logout">Sair</a></p>
+
+       <div class="alert alert-warning">
+         Você precisa verificar sua conta.
+         Entre no seu E-mail e clique na verificação no link que acabamos de enviar para seu e-mail <strong>aqui fica seu email</strong>
+       </div>
+
+       <button class="btn btn-block btn-lg btn-primary">Você está verificado!</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
